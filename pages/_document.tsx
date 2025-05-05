1e26d87781a84e2aaf358f0c3a233966605de3e9
dogsbreeder <xxzeljkoxx@gmail.com>
2025-05-05 17:48:16 +0200

    Add Bing Webmaster Tools verification meta tag

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="sr">
+      <Head>
+        <meta name="msvalidate.01" content="6644A96842A0EA4002BA88B5EA3D6D2A" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+} 
