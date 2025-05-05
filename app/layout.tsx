ab66620395c3a9a6c686b6768cd2fe6ed0c895f4
dogsbreeder <xxzeljkoxx@gmail.com>
2025-05-05 22:12:29 +0200

    fix: add Bing msvalidate meta tag directly in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,7 @@ export default function RootLayout({
   return (
     <html lang="sr">
       <head>
+        <meta name="msvalidate.01" content="6644A96842A0EA4002BA88B5EA3D6D2A" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
